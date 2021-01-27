7bfadf37e6e534ba25d5d92764826afbbc99c003
chapterjason <jason@sourecode.dev>
2021-01-27 21:43:25 +0100

    Fix postgresql dependency

--- apply-templates.sh
+++ apply-templates.sh
@@ -4,6 +4,13 @@
 PHP_VERSIONS=("7.4" "8.0")
 APCU_VERSION="5.1.19"
 
+# Detect os
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    Darwin*)    MACHINE=Mac;;
+    *)          MACHINE="Other:${UNAME_OUT}"
+esac
+
 # Cleanup
 rm -r php
 
@@ -18,11 +25,23 @@ do
   cp base.template "php/$PHP_VERSION/cli/Dockerfile"
   cp base.template "php/$PHP_VERSION/fpm/Dockerfile"
 
-  # Replace image placeholder
-  sed -i "s/{{ IMAGE }}/php:$PHP_VERSION-cli-alpine/" "php/$PHP_VERSION/cli/Dockerfile"
-  sed -i "s/{{ IMAGE }}/php:$PHP_VERSION-fpm-alpine/" "php/$PHP_VERSION/fpm/Dockerfile"
+  if [[ $MACHINE == "Mac" ]]; then
+    # Replace image placeholder
+    sed -i '' "s/{{ IMAGE }}/php:$PHP_VERSION-cli-alpine/" "php/$PHP_VERSION/cli/Dockerfile"
+    sed -i '' "s/{{ IMAGE }}/php:$PHP_VERSION-fpm-alpine/" "php/$PHP_VERSION/fpm/Dockerfile"
 
-  # Replace apcu placeholder
-  sed -i "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/fpm/Dockerfile"
-  sed -i "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/cli/Dockerfile"
+    # Replace apcu placeholder
+    sed -i '' "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/fpm/Dockerfile"
+    sed -i '' "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/cli/Dockerfile"
+  fi
+
+  if [[ $MACHINE == "Other" ]]; then
+    # Replace image placeholder
+    sed -i "s/{{ IMAGE }}/php:$PHP_VERSION-cli-alpine/" "php/$PHP_VERSION/cli/Dockerfile"
+    sed -i "s/{{ IMAGE }}/php:$PHP_VERSION-fpm-alpine/" "php/$PHP_VERSION/fpm/Dockerfile"
+
+    # Replace apcu placeholder
+    sed -i "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/fpm/Dockerfile"
+    sed -i "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/cli/Dockerfile"
+  fi
 done
