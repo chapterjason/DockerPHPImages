dbdcd7e20289b4a8200f21133dafabb174af6eb6
chapterjason <jason@sourecode.dev>
2021-02-16 19:21:43 +0100

    Add php images with nodejs installed

--- apply-templates.sh
+++ apply-templates.sh
@@ -2,46 +2,53 @@
 
 # Config
 PHP_VERSIONS=("7.4" "8.0")
+PHP_TYPES=("cli" "fpm" "cli-node" "fpm-node")
 APCU_VERSION="5.1.19"
 
 # Detect os
 UNAME_OUT="$(uname -s)"
 case "${UNAME_OUT}" in
-    Darwin*)    MACHINE=Mac;;
-    *)          MACHINE="Other:${UNAME_OUT}"
+Darwin*) MACHINE=Mac ;;
+*) MACHINE="Other:${UNAME_OUT}" ;;
 esac
 
 # Cleanup
 rm -r php
 
 # Actual generating
-for PHP_VERSION in "${PHP_VERSIONS[@]}"
-do
-  # Create directories
-  mkdir -p "php/$PHP_VERSION/cli"
-  mkdir -p "php/$PHP_VERSION/fpm"
+for PHP_VERSION in "${PHP_VERSIONS[@]}"; do
+  for PHP_TYPE in "${PHP_TYPES[@]}"; do
+    if [[ "$PHP_TYPE" == *-node ]]; then
+      REAL_TYPE=${PHP_TYPE%"-node"}
+      IMAGE="php:$PHP_VERSION-$REAL_TYPE-alpine"
+    else
+      IMAGE="php:$PHP_VERSION-$PHP_TYPE-alpine"
+    fi
 
-  # Copy templates
-  cp base.template "php/$PHP_VERSION/cli/Dockerfile"
-  cp base.template "php/$PHP_VERSION/fpm/Dockerfile"
+    FILE="php/$PHP_VERSION/$PHP_TYPE/Dockerfile"
 
-  if [[ $MACHINE == "Mac" ]]; then
-    # Replace image placeholder
-    sed -i '' "s/{{ IMAGE }}/php:$PHP_VERSION-cli-alpine/" "php/$PHP_VERSION/cli/Dockerfile"
-    sed -i '' "s/{{ IMAGE }}/php:$PHP_VERSION-fpm-alpine/" "php/$PHP_VERSION/fpm/Dockerfile"
+    # Create directory
+    mkdir -p "php/$PHP_VERSION/$PHP_TYPE"
 
-    # Replace apcu placeholder
-    sed -i '' "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/fpm/Dockerfile"
-    sed -i '' "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/cli/Dockerfile"
-  fi
+    # Copy template
+    cp base.template "$FILE"
 
-  if [[ $MACHINE == "Other" ]]; then
-    # Replace image placeholder
-    sed -i "s/{{ IMAGE }}/php:$PHP_VERSION-cli-alpine/" "php/$PHP_VERSION/cli/Dockerfile"
-    sed -i "s/{{ IMAGE }}/php:$PHP_VERSION-fpm-alpine/" "php/$PHP_VERSION/fpm/Dockerfile"
+    if [[ "$PHP_TYPE" == *-node ]]; then
+      cat node.partial.template >> "$FILE"
+    fi
 
-    # Replace apcu placeholder
-    sed -i "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/fpm/Dockerfile"
-    sed -i "s/{{ APCU }}/$APCU_VERSION/" "php/$PHP_VERSION/cli/Dockerfile"
-  fi
+    if [[ $MACHINE == "Mac" ]]; then
+      # Replace image placeholder
+      sed -i '' "s/{{ IMAGE }}/$IMAGE/" "$FILE"
+
+      # Replace apcu placeholder
+      sed -i '' "s/{{ APCU }}/$APCU_VERSION/" "$FILE"
+    else
+      # Replace image placeholder
+      sed -i "s/{{ IMAGE }}/$IMAGE/" "$FILE"
+
+      # Replace apcu placeholder
+      sed -i "s/{{ APCU }}/$APCU_VERSION/" "$FILE"
+    fi
+  done
 done
