a1037a17ec6696c2b685a1ada8680a9bd6c5c178
chapterjason <jason@sourecode.dev>
2021-11-01 20:20:36 +0100

    enhance: Add roadrunner

--- apply-templates.sh
+++ apply-templates.sh
@@ -2,7 +2,8 @@
 
 # Config
 PHP_VERSIONS=("7.4" "8.0" "8.0.3" "8.1")
-PHP_TYPES=("cli" "fpm" "cli-node" "fpm-node")
+PHP_TYPES=("cli" "fpm" "cli-node" "fpm-node" "cli-roadrunner" "cli-roadrunner-node")
+ROADRUNNER_IMAGE_VERSION="2.5.3"
 APCU_VERSION="5.1.20"
 
 # Detect os
@@ -18,11 +19,14 @@ rm -r php
 # Actual generating
 for PHP_VERSION in "${PHP_VERSIONS[@]}"; do
   for PHP_TYPE in "${PHP_TYPES[@]}"; do
-    if [[ "$PHP_TYPE" == *-node ]]; then
-      REAL_TYPE=${PHP_TYPE%"-node"}
-      IMAGE="php:$PHP_VERSION-$REAL_TYPE-alpine"
-    else
-      IMAGE="php:$PHP_VERSION-$PHP_TYPE-alpine"
+    PHP_IMAGE_VERSION="$PHP_VERSION-$PHP_TYPE-alpine"
+
+    if [[ "$PHP_TYPE" =~ "node" ]]; then
+      PHP_IMAGE_VERSION=${PHP_IMAGE_VERSION//"-node"/}
+    fi
+
+    if [[ "$PHP_TYPE" =~ "roadrunner" ]]; then
+      PHP_IMAGE_VERSION=${PHP_IMAGE_VERSION//"-roadrunner"/}
     fi
 
     FILE="php/$PHP_VERSION/$PHP_TYPE/Dockerfile"
@@ -30,25 +34,58 @@ for PHP_VERSION in "${PHP_VERSIONS[@]}"; do
     # Create directory
     mkdir -p "php/$PHP_VERSION/$PHP_TYPE"
 
-    # Copy template
-    cp base.template "$FILE"
+    touch "$FILE"
 
-    if [[ "$PHP_TYPE" == *-node ]]; then
-      cat node.partial.template >> "$FILE"
+    ##########
+    ##### Args in default order
+    ##########
+    cat "templates/args/php.args.template" >> "$FILE"
+
+    if [[ "$PHP_TYPE" =~ "roadrunner" ]]; then
+      cat "templates/args/roadrunner.args.template" >> "$FILE"
+    fi
+
+    ##########
+    ##### Pre in reverse order
+    ##########
+    if [[ "$PHP_TYPE" =~ "roadrunner" ]]; then
+      cat "templates/pre/roadrunner.pre.template" >> "$FILE"
+    fi
+
+    cat "templates/pre/php.pre.template" >> "$FILE"
+
+    ##########
+    ##### Base in default order
+    ##########
+    cat "templates/php.template" >> "$FILE"
+    cat "templates/composer.template" >> "$FILE"
+
+    if [[ "$PHP_TYPE" =~ "node" ]]; then
+      cat templates/node.template >> "$FILE"
+    fi
+
+    if [[ "$PHP_TYPE" =~ "roadrunner" ]]; then
+      cat templates/roadrunner.template >> "$FILE"
     fi
 
     if [[ $MACHINE == "Mac" ]]; then
       # Replace image placeholder
-      sed -i '' "s/{{ IMAGE }}/$IMAGE/" "$FILE"
+      sed -i '' "s/{{ PHP_IMAGE_VERSION }}/$PHP_IMAGE_VERSION/" "$FILE"
 
       # Replace apcu placeholder
       sed -i '' "s/{{ APCU }}/$APCU_VERSION/" "$FILE"
+
+      # Replace roadrunner placeholder
+      sed -i '' "s/{{ ROADRUNNER_IMAGE_VERSION }}/$ROADRUNNER_IMAGE_VERSION/" "$FILE"
     else
       # Replace image placeholder
-      sed -i "s/{{ IMAGE }}/$IMAGE/" "$FILE"
+      sed -i "s/{{ PHP_IMAGE_VERSION }}/$PHP_IMAGE_VERSION/" "$FILE"
 
       # Replace apcu placeholder
       sed -i "s/{{ APCU }}/$APCU_VERSION/" "$FILE"
+
+            # Replace roadrunner placeholder
+      sed -i "s/{{ ROADRUNNER_IMAGE_VERSION }}/$ROADRUNNER_IMAGE_VERSION/" "$FILE"
     fi
   done
 done
